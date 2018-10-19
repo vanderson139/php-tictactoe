bf24c7c141c6f39ced0825fb18c452d1552c1a68
Vanderson Lopes <vanderson139@hotmail.com>
2018-10-19 20:14:48 -0300

    determine the winner based on the possible wins

--- app/Match.php
+++ app/Match.php
@@ -16,6 +16,23 @@ class Match extends Model
         ],
     ];
 
+    const POSSIBLE_WINS_HORIZONTAL = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+    ];
+
+    const POSSIBLE_WINS_VERTICAL = [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+    ];
+
+    const POSSIBLE_WINS_DIAGONAL = [
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
     /**
      * Indicates if the model should be timestamped.
      *
@@ -53,12 +70,76 @@ class Match extends Model
         return 'Match' . $this->attributes['id'];
     }
 
+    public function getMatchWinner() {
+        if($this->theWinnerIs1()) {
+            return 1;
+        }
+
+        if($this->theWinnerIs2()) {
+            return 2;
+        }
+
+        return 0;
+    }
+
+    private function theWinnerIs1() {
+        $player = 1;
+
+        return $this->winsInHorizontal($player)
+            || $this->winsInVertical($player)
+            || $this->winsInDiagonal($player);
+    }
+
+    private function theWinnerIs2() {
+        $player = 2;
+
+        return $this->winsInHorizontal($player)
+            || $this->winsInVertical($player)
+            || $this->winsInDiagonal($player);
+    }
+
+    private function winsInHorizontal($player) {
+        $result = array_keys($this->board, $player);
+
+        foreach(self::POSSIBLE_WINS_HORIZONTAL as $win) {
+            if(count(array_intersect($result, $win)) == 3) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function winsInVertical($player) {
+        $result = array_keys($this->board, $player);
+
+        foreach(self::POSSIBLE_WINS_VERTICAL as $win) {
+            if(count(array_intersect($result, $win)) == 3) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function winsInDiagonal($player) {
+        $result = array_keys($this->board, $player);
+
+        foreach(self::POSSIBLE_WINS_DIAGONAL as $win) {
+            if(count(array_intersect($result, $win)) == 3) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public function toArray()
     {
         $attributes = parent::toArray();
 
         $attributes['name'] = $this->getMatchName();
-        $attributes['winner'] = 0;
+        $attributes['winner'] = $this->getMatchWinner();
 
         return $attributes;
     }
